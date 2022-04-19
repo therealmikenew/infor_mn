a9053ea39a6ad1440864be30bf577329e3986b37
Michael Newman <therealmichaelnewman@gmail.com>
2022-04-18 21:38:43 -0400

    update file structure

--- infor_mn/src/components/BookGrid.jsx
+++ infor_mn/src/components/BookGrid.jsx
@@ -1,5 +1,5 @@
 import { DataGrid } from "@mui/x-data-grid"
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 const BookGrid = ({results}) => {
   let navigate = useNavigate()
@@ -34,39 +34,21 @@ const BookGrid = ({results}) => {
         },
       ];
 
-
       const rows = results.map((res, idx)=>(
           { id: idx, title: res.volumeInfo.title, authors: res.volumeInfo.authors, retail_price: res.saleInfo.saleability === "FOR_SALE" ? (res.saleInfo.retailPrice.amount): ("No price listed"), page_count: res.volumeInfo.pageCount, categories: res.volumeInfo.categories }
       ))
 
-      const handleClick = (e) => {
-        navigate(`/${e.id}`)
-      }
-
-
     return (
         <div style={{ width: 900, height: 400 }}>
-
-        
-            <DataGrid
+          <DataGrid
             columns={columns}
             rows={rows}
-            onRowClick={(e)=>(handleClick(e))}
+            onRowClick={(e)=>(navigate(`/${e.id}`))}
             pageSize={5}
-        rowsPerPageOptions={[5]}
-        
-        disableSelectionOnClick/>
-
-
-          
-
-
-
-           
+            rowsPerPageOptions={[5]}
+            disableSelectionOnClick/> 
         </div>
     )
-
-
 }
 
 export default BookGrid
